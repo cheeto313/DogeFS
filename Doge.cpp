3e409b59fb8867ef4778e9243f2b252e57c077ca
tatumd <devan.tatum@gmail.com>
2014-03-13 16:44:54 -0700

    AAAaaaaannd more things..

---
+++ Doge.cpp
@@ -0,0 +1,25 @@
+/**
+Devan Tatum
+
+Such Driver, very demo, amaze, wow.
+*/
+
+#include "Inode.h"
+#include "FileSystem.h"
+#include "Disk.h"
+
+using std::cout
+using std::cin
+
+int main(int argc, char* argv[]){
+
+
+	/*
+		Steps in the system
+			1. Create file
+			2. Write to the file
+			3. Read from file
+			4. Display the file properties
+	*/
+
+}
